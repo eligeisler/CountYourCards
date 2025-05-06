bd3b455d80bffe5cf978c7c9893dda3601a53c2a
eligeisler <eligeisler@tsn.at>
2025-05-06 16:26:27 +0200

    MainPage finished

--- CountYourCards/Views/MainPage.xaml.cs
+++ CountYourCards/Views/MainPage.xaml.cs
@@ -1,11 +1,15 @@
-﻿namespace CountYourCards.Views
+﻿using CountYourCards.ViewModels;
+using System.Threading.Tasks;
+
+namespace CountYourCards.Views
 {
     public partial class MainPage : ContentPage
     {
 
-        public MainPage()
+        public MainPage(MainPageViewModel vm)
         {
             InitializeComponent();
+            this.BindingContext = vm;
         }
     }
 
